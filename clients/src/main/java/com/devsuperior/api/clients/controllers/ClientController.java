721cf5c0061e3d8f7f054399008bad293dfbe5e7
oliveiralex <alexandre.si.ufu@gmail.com>
2023-01-16 09:52:34 -0300

    GET Clients paged

--- clients/src/main/java/com/devsuperior/api/clients/controllers/ClientController.java
+++ clients/src/main/java/com/devsuperior/api/clients/controllers/ClientController.java
@@ -1,14 +1,14 @@
 package com.devsuperior.api.clients.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.api.clients.entities.Client;
+import com.devsuperior.api.clients.dto.ClientDTO;
 import com.devsuperior.api.clients.services.ClientService;
 
 @RestController
@@ -19,12 +19,12 @@ public class ClientController {
 	private ClientService service;
 	
 	@GetMapping
-	public List<Client> findAll() {
-		return service.findAll();
+	public Page<ClientDTO> findAll(Pageable pageable) {
+		return service.findAll(pageable);
 	}
 
 	@GetMapping(value = "/{id}")
-	public Client findById(@PathVariable Long id) {
+	public ClientDTO findById(@PathVariable Long id) {
 		return service.findById(id);
 	}
 }
--- clients/src/main/java/com/devsuperior/api/clients/services/ClientService.java
+++ clients/src/main/java/com/devsuperior/api/clients/services/ClientService.java
@@ -1,11 +1,12 @@
 package com.devsuperior.api.clients.services;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.api.clients.dto.ClientDTO;
 import com.devsuperior.api.clients.entities.Client;
 import com.devsuperior.api.clients.repositories.ClientRepository;
 
@@ -16,12 +17,14 @@ public class ClientService {
 	private ClientRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Client> findAll() {
-		return repository.findAll();
+	public Page<ClientDTO> findAll(Pageable pageable) {
+		Page<Client> result = repository.findAll(pageable);
+		return result.map(x -> new ClientDTO(x));
 	}
 	
 	@Transactional(readOnly = true)
-	public Client findById(Long id) {
-		return repository.findById(id).get();
+	public ClientDTO findById(Long id) {
+		Client entity = repository.findById(id).get();
+		return new ClientDTO(entity);
 	}
 }
