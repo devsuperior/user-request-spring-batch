fb2bbdeb0d1f516cd85b1542be40b5849e56b566
Alexandre Oliveira <alexandreoliveira@MacBookAir.local>
2023-09-06 15:32:45 -0300

    Read and save data using PostgresSQL

---
+++ github_user_postgresql/src/main/java/com/devsuperior/githubuser/GithubUserPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.githubuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubUserPostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubUserPostgresqlApplication.class, args);
+	}
+
+}
